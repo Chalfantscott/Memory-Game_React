2c376832223b9fa06cf98791d1e86adbabd8eec9
Scott Chalfant <chalfantscott@Scotts-MacBook-Pro-2.local>
2018-06-28 11:47:41 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <h1 className="App-title">Memory Game</h1>
+          <h3 className="sub-App-title">Match Cards to Win</h3>
+
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
