53c2caba71e34b6b0b6954bb2a99f1f6b55b618d
Scott Chalfant <chalfantscott@Scotts-MacBook-Pro-2.local>
2018-06-28 18:15:11 -0400

    aesthetic complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import MemoryCard from './MemoryCard.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+        
           <h1 className="App-title">Memory Game</h1>
           <h3 className="sub-App-title">Match Cards to Win</h3>
 
         </header>
+        <div className="cardSpace"><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/><MemoryCard/></div>
+        
         
       </div>
     );
---
+++ src/MemoryCard.js
@@ -0,0 +1,20 @@
+//Using App.js as a reference, define a react Component class and export it
+//Name the class “MemoryCard”
+//Make sure to include React and { Component } at the top
+//Make sure you include the “export default” statement at the bottom
+
+import React, { Component } from 'react';
+
+
+class MemoryCard extends Component {
+    render() {
+        return (
+            <div className="MemoryCard">
+                <img src='DigitalCrafts-Logo-Wrench.png' alt='digitalcrafts logo' />
+                
+            </div>
+        )
+    }
+};
+
+export default MemoryCard;
